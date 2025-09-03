3c9fe351f65c0fd4b6dff549b73cc33b5fac96b2
renatxat <khatymov.rr@phystech.edu>
2025-09-03 05:17:32 +0300

    init github pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 /* eslint-disable prettier/prettier */
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   context: path.join(__dirname, 'src'),
   entry: ['./main.js'],
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: '[name].[hash].js'
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: '/lectoriy-previews/',
+    filename: '[name].[contenthash].js',
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: 'index.html'
+      template: './src/index.html',
+      filename: 'index.html',
     })
   ],
   resolve: {
